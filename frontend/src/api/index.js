9174422a7b5c9857d03248372123f00e33768075
IIIjong <pjwfish@naver.com>
2025-10-20 02:38:42 +0900

    리프레시 토큰 호출 수정

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -9,53 +9,48 @@ const apiClient = axios.create({
 });
 
 apiClient.interceptors.request.use(
+  (config) => {
+    const authStore = useAuthStore();
+    const accessToken = authStore.accessToken;
 
-    (config) =>{
-        const authStore = useAuthStore();
-        const accessToken = authStore.accessToken;
-
-        if(accessToken){
-            config.headers.Authorization = `Bearer ${accessToken}`;
-        }
-        
-        return config;
-    },
-    (error) =>{
-        return Promise.reject(error);
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
     }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
-
 apiClient.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (error) => {
-        const orignalRequest = error.config;
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    const authStore = useAuthStore();
 
-        if (error.response.status === 401 && !orignalRequest._retry) {
-            orignalRequest._retry = true;
-            const authStore = useAuthStore();
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
 
-            try {
-                const refreshResponse = await axios.post('/api/v1/auth/refresh');
-                const newAccessToken = refreshResponse.data.accessToken;
+      try {
+        const refreshResponse = await apiClient.post("/api/v1/auth/refresh");
+        const newAccessToken = refreshResponse.data.accessToken;
 
-                authStore.setAccessToken(newAccessToken);
-                
-                orignalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-                console.log("액세스 토큰 갱신 성공");
-                return apiClient(orignalRequest);
-            } catch (refreshError) {
-                
-                authStore.logout();
-                console.log("리프레시 토큰 만료, 갱신 실패");
-                router.push('/');
-                return Promise.reject(refreshError);
-            }
-        }
-        return Promise.reject(error);
+        authStore.setAccessToken(newAccessToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        console.log("액세스 토큰 갱신 성공");
+
+        return apiClient(originalRequest);
+      } catch (refreshError) {
+        console.warn("리프레시 토큰 만료 또는 갱신 실패");
+        authStore.logout();
+        router.push("/");
+        return Promise.reject(refreshError);
+      }
     }
+
+    return Promise.reject(error);
+  }
 );
 
 export default apiClient;
